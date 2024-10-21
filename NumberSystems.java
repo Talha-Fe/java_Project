8405f21c849c9acc780287b498a27c6ef0312b5a
Talha Demir <118924543+Talha-Fe@users.noreply.github.com>
2024-10-21 13:51:09 +0300

    Update 0.32.2 NumberSystems.java

--- NumberSystems.java
+++ NumberSystems.java
@@ -11,6 +11,7 @@ public class NumberSystems {
 		
 		int choice = userIn.nextInt();
 		
+		// BinDec
 		if(choice == 1) {
 			
 			System.out.println("Binary to Decimal(1) # Decimal to Binary(2)");
@@ -24,7 +25,7 @@ public class NumberSystems {
 				int b1 = binToDec            % 10; // 2^0
 				int b2 = binToDec / 10       % 10; 
 				int b3 = binToDec / 100      % 10;
-				int b4 = binToDec / 1000     % 10; // Expml. 10101010
+				int b4 = binToDec / 1000     % 10;
 				int b5 = binToDec / 10000    % 10;
 				int b6 = binToDec / 100000   % 10;
 				int b7 = binToDec / 1000000  % 10;
@@ -48,13 +49,25 @@ public class NumberSystems {
 				
 				System.out.println("Please type an decimal code(DO NOT type higher then 255!): ");
 				int decToBin = userIn.nextInt();
-				int resutBin = decToBin;
-				
-				while(decToBin > 0) {
+				int a; //left side
+				int b; //  I
+				int c; //  I
+				int d; //  I
+				int e; //  I
+				int f; //  I
+				int g; //  I
+				int h; //right side
 					
+				a = decToBin       % 2;
+				b = decToBin / 2   % 2;
+				c = decToBin / 4   % 2;
+				d = decToBin / 8   % 2;
+				e = decToBin / 16  % 2;
+				f = decToBin / 32  % 2;
+				g = decToBin / 64  % 2;
+				h = decToBin / 128 % 2;
 					
-					
-				}
+				System.out.println("Binary Result: " + a + b + c + d + e + f + g + h);
 				
 			}
 			
@@ -65,10 +78,65 @@ public class NumberSystems {
 			}
 		}
 		
+		
+		// BinHex
 		else if(choice == 2) {
+			
+			System.out.println("Binary to Hexadecimal(1) # Hexadecimal to Binary(2)");
+			int choice_BH = userIn.nextInt();
+			
+			if(choice_BH == 1) {
+				
+				System.out.println("Please type an binary code(DO NOT type higher then 1 BYTE!): ");
+				int binToHex = userIn.nextInt(); 
+				
+			    String H10 = "A";
+				String H11 = "B";
+				String H12 = "C";
+				String H13 = "D";
+				String H14 = "E";
+				String H15 = "F";
+				
+				int r1 = binToHex            % 10; // 2^0
+				int r2 = binToHex / 10       % 10; 
+				int r3 = binToHex / 100      % 10;
+				int r4 = binToHex / 1000     % 10;
+				int l1 = binToHex / 10000    % 10;
+				int l2 = binToHex / 100000   % 10;
+				int l3 = binToHex / 1000000  % 10;
+				int l4 = binToHex / 10000000 % 10; //2^7
+				
+				r1 = r1 * (int) Math.pow(2, 0);
+				r2 = r2 * (int) Math.pow(2, 1);
+				r3 = r3 * (int) Math.pow(2, 2);
+				r4 = r4 * (int) Math.pow(2, 3);
+				
+				l1 = l4 * (int) Math.pow(2, 0);
+				l2 = l2 * (int) Math.pow(2, 1);
+				l3 = l3 * (int) Math.pow(2, 2);
+				l4 = l4 * (int) Math.pow(2, 3);
+				
+				int hexaDecR = r1 + r2 + r3 + r4; //right side of the hex 
+				int hexaDecL = l1 + l2 + l3 + l4; //left side of the bin
+				
+				System.out.println("Hexadecimal Result: ");
+				
+			}
+			
+			else if(choice_BH == 2) {
+				
+			}
+			
+			else {
+				
+				System.out.println("Error! Please use only 1 or 2.");
+				
+			}
 			
 		}
 		
+		
+		// HexDec
 		else if(choice == 3) {
 			
 		}
